ff58bf0e53fd502c66f3858dcba784ffb86a7a10
AtonCode <49330231+AtonCode@users.noreply.github.com>
2022-02-08 23:21:48 -0500

    Entrega0

---
+++ Modulos/ManejadorComandos.h
@@ -0,0 +1,8 @@
+#ifndef ManejadorComandos_h 
+#define ManejadorComandos_h 
+
+void manejadorComandos();
+bool helpComands();
+void clearTerminal(char os);
+
+#endif /* ManejadorComandos_h */
---
+++ main.cpp
@@ -0,0 +1,20 @@
+// g++ -std=c++11 Modulos/ManejadorComandos.cpp main.cpp -o EstructurasProyecto && ./EstructurasProyecto
+/*
+Gupo 2
+Abril Cano & Alejandro Sacristan & Humberto Rueda
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include "Modulos/ManejadorComandos.h"
+
+using namespace std;
+
+int main() {
+  
+  clearTerminal('l');
+  manejadorComandos();
+
+  return -1;
+}
